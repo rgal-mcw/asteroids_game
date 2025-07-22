2f2ba55ee77dadf1695e5e6bd31a5b2612786e54
rgal-mcw <ryan.biostat@gmail.com>
2025-07-21 19:31:44 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(F"Screen height: {SCREEN_HEIGHT}")
+    screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen,"black")
+        
+
+
+        pygame.display.flip()
+    return
+
+if __name__ == "__main__":
+    main()
